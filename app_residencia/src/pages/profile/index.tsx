75d418d08281011fc0192b69cd46512f216f9134
Talles Dias <tallesdelfino1@gmail.com>
2022-07-04 20:13:02 -0300

    juntando algumas paginas

---
+++ app_residencia/src/pages/profile/index.tsx
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react';
+import { View, StyleSheet, Alert, Image } from "react-native";
+import { Text } from "react-native-elements";
+import { AutenticacaoContext } from '../../context/AutenticacaoContext';
+
+const Profile = () => {
+
+    const { usuario } = useContext(AutenticacaoContext);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.circulo}>
+                <Image style={styles.fotoPerfil} source={require('../../assets/bookland.png')} />
+            </View>
+            <View style={styles.square}>
+                <View style={styles.views_do_nome_email}>
+                    <Text style={styles.textName}>ID de usuario:</Text>
+                    <Text style={styles.textName2}>{usuario.id}</Text>
+                </View>
+                <View style={styles.views_do_nome_email}>
+                    <Text style={styles.textName}>Email:</Text>
+                    <Text style={styles.textName2}>{usuario.email}</Text>
+                </View>
+                <View style={styles.nome_e_email}>
+                    <View style={styles.views_do_nome_email}>
+                        <Text style={styles.textName}>Nome:</Text>
+                        <Text style={styles.textName2}>{usuario.name}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#A2717C',
+        padding: 16,
+        alignItems: 'center',
+
+    },
+    circulo: {
+        backgroundColor: '#ffffff',
+        borderRadius: 150,
+        width: 250,
+        height: 250,
+    },
+    square: {
+        backgroundColor: '#562637',
+        width: '100%',
+        height: 250,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+    },
+    nome_e_email: {
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#562637',
+    },
+    button_edit: {
+        backgroundColor: '#A2717C',
+        marginTop: 10,
+        cursor: 'pointer',
+    },
+    button_editTittle: {
+        fontSize: 20,
+    },
+    inputContainerName: {
+        backgroundColor: '#ffffff',
+        width: '100%',
+        marginTop: 10,
+    },
+    inputContainerEmail: {
+        backgroundColor: '#ffffff',
+        width: '100%',
+        marginTop: 10,
+    },
+    textName: {
+        color: '#121212',
+        backgroundColor: '#fff',
+        fontWeight: 'bold',
+    },
+    textName2: {
+        color: '#121212',
+        backgroundColor: '#fff',
+    },
+    fotoPerfil: {
+        borderRadius: 150,
+        width: 250,
+        height: 250,
+    },
+    views_do_nome_email: {
+        backgroundColor: '#ffffff',
+        padding: 10,
+        width: 300,
+        height: 60,
+        marginTop: 15,
+    }
+});
+
+export default Profile;
